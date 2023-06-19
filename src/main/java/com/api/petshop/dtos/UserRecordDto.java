e1062a8361d2962bb380d9cbad3dcb962c793855
Lucas Sousa <lucas.sousa_99@hotmail.com>
2023-06-19 20:42:14 -0300

    criando API RESTful PetShop

--- src/main/java/com/api/petshop/dtos/UserRecordDto.java
+++ src/main/java/com/api/petshop/dtos/UserRecordDto.java
@@ -4,6 +4,9 @@ package com.api.petshop.dtos;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record UserRecordDto( String username,  String cpf) {
-
+public record UserRecordDto(
+        @NotBlank String username,
+        @NotNull String cpf,
+        @NotBlank String password
+) {
 }
