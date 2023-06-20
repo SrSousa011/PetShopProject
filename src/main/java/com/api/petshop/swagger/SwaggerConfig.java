a78086584f7f0895fa89a3429663fbcff6eaf582
Lucas Sousa <lucas.sousa_99@hotmail.com>
2023-06-20 20:22:26 -0300

    [ADD] Adicionando documentação swagger

--- src/main/java/com/api/petshop/swagger/SwaggerConfig.java
+++ src/main/java/com/api/petshop/swagger/SwaggerConfig.java
@@ -3,24 +3,25 @@ package com.api.petshop.swagger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 import java.util.Collections;
 
 @Configuration
-@EnableSwagger2
+@EnableSwagger2WebMvc
 public class SwaggerConfig {
 
     private static final Contact DEFAULT_CONTACT = new Contact(
             "Lucas Sousa", "https://github.com/SrSousa011/", "lucas.sousa_99@hotmail.com");
 
     private static final ApiInfo DEFAULT_API_INFO = new ApiInfo(
-            "PetShop API", "Sistema de controle de atendimentos de uma petshop", "1.0",
+            "PetShop API", "Sistema de controle de atendimentos de um petshop", "1.0",
             "urn:tos", DEFAULT_CONTACT,
             "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0", Collections.emptyList()
     );
@@ -31,6 +32,7 @@ public class SwaggerConfig {
                 .apiInfo(DEFAULT_API_INFO)
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
                 .build();
     }
 }
